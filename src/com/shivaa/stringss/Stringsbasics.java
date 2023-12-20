c55a38fa19b902d96c34edbcea45bf5fa1346fbe
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-12-20 16:27:04 +0530

    stringfile

---
+++ src/com/shivaa/stringss/Stringsbasics.java
@@ -0,0 +1,18 @@
+package com.shivaa.stringss;
+
+import java.util.*;
+public class Stringsbasics {
+    public static void main(String[] args) {
+        // this how you define a string
+        String s = "hello world";
+        System.out.println(s);
+
+        Scanner scn = new Scanner(System.in);
+        // how to take input form user but it will run only till space in input so use nextLine();
+//        String str = scn.next();
+//        System.out.println(str);
+
+        String str1 = scn.nextLine();
+        System.out.println(str1);
+    }
+}
