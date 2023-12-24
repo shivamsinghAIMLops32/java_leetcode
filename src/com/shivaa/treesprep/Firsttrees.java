daf6c84dfe7585448b7b376f0f09a34c9d1eb21a
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-12-25 00:10:17 +0530

    trees traversal

--- src/com/shivaa/treesprep/Firsttrees.java
+++ src/com/shivaa/treesprep/Firsttrees.java
@@ -1,16 +1,18 @@
 package com.shivaa.treesprep;
 
 public class Firsttrees {
-    public static class Node{
+    public static class Node{        // creating a tree node class store left,right,data
         Node left;
         Node right;
         int data;
-        Node(int data){
+        Node(int data){          // constructor of Tree node class
             this.data = data;
+            left=null;
+            right=null;
         }
 
         public static void main(String[] args) {
-            Node root = new Node(5);
+            Node root = new Node(5);       // creating objects with data value
 //            root.left =root1;
             Node child1 = new Node(7);
             Node child2 = new Node(19);
@@ -30,17 +32,36 @@ public class Firsttrees {
             child3.right = child5;
             child5.left = child6;
 
-            traverse(root);
+            traversePreorder(root);
+ //           traversePostorder(root);
+ //           traverseInorder(root);
         }
-        public static void traverse(Node root){
+        public static void traversePreorder(Node root){
             if (root == null) {
                 return;
              }
-//            System.out.print(root.data + " -> ");      5 -> 7 -> 40 -> 22 -> 23 -> 19 -> 40 -> 22 -> 23 -> 6 ->
-            traverse(root.left);
-//          System.out.print(root.data + " -> ");        40 -> 23 -> 22 -> 7 -> 5 -> 40 -> 23 -> 22 -> 19 -> 6 ->
-            traverse(root.right);
-//            System.out.print(root.data + " -> ");      23 -> 22 -> 40 -> 7 -> 23 -> 22 -> 40 -> 6 -> 19 -> 5 ->
+            //preorder
+            System.out.print(root.data + " -> ");       //5 -> 7 -> 40 -> 22 -> 23 -> 19 -> 40 -> 22 -> 23 -> 6 ->
+            traversePreorder(root.left);
+            traversePreorder(root.right);
+        }
+        public static void traversePostorder(Node root){
+            if (root == null) {
+                return;
+            }
+            traversePostorder(root.left);
+            traversePreorder(root.right);
+            // postorder
+            System.out.print(root.data + " -> ");      //23 -> 22 -> 40 -> 7 -> 23 -> 22 -> 40 -> 6 -> 19 -> 5 ->
+        }
+        public static void traverseInorder(Node root){
+            if (root == null) {
+                return;
+            }
+            traverseInorder(root.left);
+            //inorder
+            System.out.print(root.data + " -> ");        //40 -> 23 -> 22 -> 7 -> 5 -> 40 -> 23 -> 22 -> 19 -> 6 ->
+            traverseInorder(root.right);
         }
     }
 }
